a01baa00cb4450364d392d43518d894dbb844233
Felipe Munhoz Afonso <fmunhozafonso@gmail.com>
2021-11-10 17:29:05 -0300

    base configurations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { NasaDailyFeedBase } from '../types/nasa'
 
-const Home: NextPage = () => {
+const Home: NextPage<{ data?: NasaDailyFeedBase }> = (props) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -18,8 +19,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
@@ -33,10 +33,7 @@ const Home: NextPage = () => {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -46,9 +43,7 @@ const Home: NextPage = () => {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -70,3 +65,12 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch(
+    `https://api.nasa.gov/neo/rest/v1/feed/today?detailed=true&api_key=${process.env.NEO_NASA_KEY}`,
+  )
+  const data = (await res.json()) as NasaDailyFeedBase
+  if (res.status !== 200) return { notFound: true }
+  else return { props: { data } }
+}
